8aef9f8668fcca277abc843f4d89cbb18273d54e
hayitmurod707 <hayitmurod707@gmail.com>
2023-01-14 21:52:13 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import Notifications, { showNotification } from './Notifications';
+import styled from "styled-components";
+import Notifications, { showNotification } from "./Notifications";
 const Button = styled.button`
 	border-radius: 8px;
 	border: none;
@@ -14,122 +14,122 @@ const Button = styled.button`
 const App = () => (
 	<>
 		<section>
-			<h1 style={{ textAlign: 'center' }}>
+			<h1 style={{ textAlign: "center" }}>
 				Notifications component with react-toastify
 			</h1>
-			<h1 style={{ textAlign: 'center' }}>
+			<h1 style={{ textAlign: "center" }}>
 				<Button
-					style={{ backgroundColor: '#0000ff' }}
+					style={{ backgroundColor: "#0000ff" }}
 					onClick={() =>
-						showNotification({ message: 'Info message', type: 'info' })
+						showNotification({ message: "Info message", type: "info" })
 					}
 				>
 					Info
 				</Button>
 				<Button
-					style={{ backgroundColor: '#0000ff' }}
+					style={{ backgroundColor: "#0000ff" }}
 					onClick={() =>
 						showNotification({
 							autoClose: true,
-							message: 'Info message',
-							type: 'info',
+							message: "Info message",
+							type: "info",
 						})
 					}
 				>
 					Auto Close Info
 				</Button>
 			</h1>
-			<h1 style={{ textAlign: 'center' }}>
+			<h1 style={{ textAlign: "center" }}>
 				<Button
-					style={{ backgroundColor: '#ff0000' }}
+					style={{ backgroundColor: "#ff0000" }}
 					onClick={() =>
-						showNotification({ message: 'Error message', type: 'error' })
+						showNotification({ message: "Error message", type: "error" })
 					}
 				>
 					Error
 				</Button>
 				<Button
-					style={{ backgroundColor: '#ff0000' }}
+					style={{ backgroundColor: "#ff0000" }}
 					onClick={() =>
 						showNotification({
 							autoClose: true,
-							message: 'Error message',
-							type: 'error',
+							message: "Error message",
+							type: "error",
 						})
 					}
 				>
 					Auto Close Error
 				</Button>
 			</h1>
-			<h1 style={{ textAlign: 'center' }}>
+			<h1 style={{ textAlign: "center" }}>
 				<Button
-					style={{ backgroundColor: '#ffff00' }}
+					style={{ backgroundColor: "#ffff00" }}
 					onClick={() =>
 						showNotification({
-							message: 'Warning message',
-							type: 'warning',
+							message: "Warning message",
+							type: "warning",
 						})
 					}
 				>
 					Warning
 				</Button>
 				<Button
-					style={{ backgroundColor: '#ffff00' }}
+					style={{ backgroundColor: "#ffff00" }}
 					onClick={() =>
 						showNotification({
 							autoClose: true,
-							message: 'Warning message',
-							type: 'warning',
+							message: "Warning message",
+							type: "warning",
 						})
 					}
 				>
 					Auto Close Warning
 				</Button>
 			</h1>
-			<h1 style={{ textAlign: 'center' }}>
+			<h1 style={{ textAlign: "center" }}>
 				<Button
-					style={{ backgroundColor: '#008000' }}
+					style={{ backgroundColor: "#008000" }}
 					onClick={() =>
 						showNotification({
-							message: 'Success message',
-							type: 'success',
+							message: "Success message",
+							type: "success",
 						})
 					}
 				>
 					Success
 				</Button>
 				<Button
-					style={{ backgroundColor: '#008000' }}
+					style={{ backgroundColor: "#008000" }}
 					onClick={() =>
 						showNotification({
 							autoClose: true,
-							message: 'Success message',
-							type: 'success',
+							message: "Success message",
+							type: "success",
 						})
 					}
 				>
 					Auto Close Success
 				</Button>
 			</h1>
-			<h1 style={{ textAlign: 'center' }}>
+			<h1 style={{ textAlign: "center" }}>
 				<Button
-					style={{ backgroundColor: 'grey' }}
+					style={{ backgroundColor: "grey" }}
 					onClick={() =>
 						showNotification({
-							message: 'Default message',
-							type: 'default',
+							message: "Default message",
+							type: "default",
 						})
 					}
 				>
 					Default
 				</Button>
 				<Button
-					style={{ backgroundColor: 'grey' }}
+					style={{ backgroundColor: "grey" }}
 					onClick={() =>
 						showNotification({
 							autoClose: true,
-							message: 'Default message',
-							type: 'default',
+							message: "Default message",
+							type: "default",
 						})
 					}
 				>
--- src/Notifications.jsx
+++ src/Notifications.jsx
@@ -1,7 +1,7 @@
-import { string } from 'prop-types';
-import { Bounce, toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import styled, { createGlobalStyle } from 'styled-components';
+import { string } from "prop-types";
+import { Bounce, toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styled, { createGlobalStyle } from "styled-components";
 // danger color #ff0000
 // info color #0000ff
 // light color #ffffff
@@ -38,12 +38,12 @@ const defaultOptions = {
 	transition: Bounce, // oneOf [Slide, Bounce, Zoom, Flip]
 };
 const Styles = createGlobalStyle`
-   :root {
-      --toastify-icon-color-error: #ff0000;
-      --toastify-icon-color-info: #0000ff;
-      --toastify-icon-color-success: #008000;
-      --toastify-icon-color-warning: #ffff00;
-   }
+	:root {
+		--toastify-icon-color-error: #ff0000;
+		--toastify-icon-color-info: #0000ff;
+		--toastify-icon-color-success: #008000;
+		--toastify-icon-color-warning: #ffff00;
+	}
 `;
 const StyledElement = styled.section`
 	& .Toastify__toast-container {
@@ -55,7 +55,7 @@ const StyledElement = styled.section`
 			box-shadow: 0 1px 10px 0 rgba(13, 46, 105, 0.1),
 				0 1px 10px 0 rgba(13, 46, 105, 0.1);
 			color: #000000;
-			font-family: 'Gilroy', sans-serif !important;
+			font-family: "Gilroy", sans-serif !important;
 			font-size: 15px;
 			font-weight: 600;
 			padding: 14px;
@@ -81,19 +81,19 @@ const StyledElement = styled.section`
 				right: -10px;
 				top: -10px;
 				width: 21px;
-				&[data-type='success'] {
+				&[data-type="success"] {
 					background-color: #008000;
 				}
-				&[data-type='info'] {
+				&[data-type="info"] {
 					background-color: #0000ff;
 				}
-				&[data-type='error'] {
+				&[data-type="error"] {
 					background-color: #ff0000;
 				}
-				&[data-type='warning'] {
+				&[data-type="warning"] {
 					background-color: #ffff00;
 				}
-				&[data-type='default'] {
+				&[data-type="default"] {
 					background-color: #808080;
 				}
 			}
@@ -117,16 +117,16 @@ const StyledElement = styled.section`
 	}
 `;
 const CloseButton = ({ type }) => (
-	<button className='Toastify__toast-close' data-type={type}>
-		<svg width='10' height='10' viewBox='0 0 14 14' fill='none'>
+	<button className="Toastify__toast-close" data-type={type}>
+		<svg width="10" height="10" viewBox="0 0 14 14" fill="none">
 			<path
-				d='M8.40994 7.00019L12.7099 2.71019C12.8982 2.52188 13.004 2.26649 13.004 2.00019C13.004 1.73388 12.8982 1.47849 12.7099 1.29019C12.5216 1.10188 12.2662 0.996094 11.9999 0.996094C11.7336 0.996094 11.4782 1.10188 11.2899 1.29019L6.99994 5.59019L2.70994 1.29019C2.52164 1.10188 2.26624 0.996094 1.99994 0.996094C1.73364 0.996094 1.47824 1.10188 1.28994 1.29019C1.10164 1.47849 0.995847 1.73388 0.995847 2.00019C0.995847 2.26649 1.10164 2.52188 1.28994 2.71019L5.58994 7.00019L1.28994 11.2902C1.19621 11.3831 1.12182 11.4937 1.07105 11.6156C1.02028 11.7375 0.994141 11.8682 0.994141 12.0002C0.994141 12.1322 1.02028 12.2629 1.07105 12.3848C1.12182 12.5066 1.19621 12.6172 1.28994 12.7102C1.3829 12.8039 1.4935 12.8783 1.61536 12.9291C1.73722 12.9798 1.86793 13.006 1.99994 13.006C2.13195 13.006 2.26266 12.9798 2.38452 12.9291C2.50638 12.8783 2.61698 12.8039 2.70994 12.7102L6.99994 8.41019L11.2899 12.7102C11.3829 12.8039 11.4935 12.8783 11.6154 12.9291C11.7372 12.9798 11.8679 13.006 11.9999 13.006C12.132 13.006 12.2627 12.9798 12.3845 12.9291C12.5064 12.8783 12.617 12.8039 12.7099 12.7102C12.8037 12.6172 12.8781 12.5066 12.9288 12.3848C12.9796 12.2629 13.0057 12.1322 13.0057 12.0002C13.0057 11.8682 12.9796 11.7375 12.9288 11.6156C12.8781 11.4937 12.8037 11.3831 12.7099 11.2902L8.40994 7.00019Z'
-				fill='#ffffff'
+				d="M8.40994 7.00019L12.7099 2.71019C12.8982 2.52188 13.004 2.26649 13.004 2.00019C13.004 1.73388 12.8982 1.47849 12.7099 1.29019C12.5216 1.10188 12.2662 0.996094 11.9999 0.996094C11.7336 0.996094 11.4782 1.10188 11.2899 1.29019L6.99994 5.59019L2.70994 1.29019C2.52164 1.10188 2.26624 0.996094 1.99994 0.996094C1.73364 0.996094 1.47824 1.10188 1.28994 1.29019C1.10164 1.47849 0.995847 1.73388 0.995847 2.00019C0.995847 2.26649 1.10164 2.52188 1.28994 2.71019L5.58994 7.00019L1.28994 11.2902C1.19621 11.3831 1.12182 11.4937 1.07105 11.6156C1.02028 11.7375 0.994141 11.8682 0.994141 12.0002C0.994141 12.1322 1.02028 12.2629 1.07105 12.3848C1.12182 12.5066 1.19621 12.6172 1.28994 12.7102C1.3829 12.8039 1.4935 12.8783 1.61536 12.9291C1.73722 12.9798 1.86793 13.006 1.99994 13.006C2.13195 13.006 2.26266 12.9798 2.38452 12.9291C2.50638 12.8783 2.61698 12.8039 2.70994 12.7102L6.99994 8.41019L11.2899 12.7102C11.3829 12.8039 11.4935 12.8783 11.6154 12.9291C11.7372 12.9798 11.8679 13.006 11.9999 13.006C12.132 13.006 12.2627 12.9798 12.3845 12.9291C12.5064 12.8783 12.617 12.8039 12.7099 12.7102C12.8037 12.6172 12.8781 12.5066 12.9288 12.3848C12.9796 12.2629 13.0057 12.1322 13.0057 12.0002C13.0057 11.8682 12.9796 11.7375 12.9288 11.6156C12.8781 11.4937 12.8037 11.3831 12.7099 11.2902L8.40994 7.00019Z"
+				fill="#ffffff"
 			/>
 		</svg>
 	</button>
 );
-CloseButton.defaultProps = { type: 'info' };
+CloseButton.defaultProps = { type: "info" };
 CloseButton.propTypes = { type: string };
 const Notifications = () => (
 	<StyledElement>
@@ -136,17 +136,18 @@ const Notifications = () => (
 );
 export const showNotification = ({
 	autoClose = false,
-	message = 'Not entered message',
-	type = 'info',
+	message = "Not entered message",
+	type = "info",
 }) => {
 	const options = autoClose
-		? { autoClose: 5000, closeButton: false, style: { overflow: 'hidden' } }
+		? { autoClose: 5000, closeButton: false, style: { overflow: "hidden" } }
 		: {
 				autoClose: false,
 				closeButton: <CloseButton />,
-				style: { overflow: 'initial' },
+				style: { overflow: "initial" },
 		  };
-	if (['error', 'info', 'success', 'warning'].includes(type)) {
+	const hasType = ["error", "info", "success", "warning"].includes(type);
+	if (hasType) {
 		toast[type](message, options);
 	} else {
 		toast(message, options);
