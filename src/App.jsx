dbcf3952430eb6249e6093545c85b7b20bb09d41
hayitmurod707 <hayitmurod707@gmail.com>
2023-05-15 13:57:39 +0500

    ferfe

--- src/App.jsx
+++ src/App.jsx
@@ -1,169 +1,170 @@
-import styled from "styled-components";
-import Notifications, { notification } from "./Notifications";
+import styled from 'styled-components';
+import Notifications from './Notifications';
+import notification from './notification';
 const Button = styled.button`
-	border-radius: 8px;
-	border: none;
-	color: #ffffff;
-	cursor: pointer;
-	font-size: 16px;
-	height: 48px;
-	margin: 0 10px;
-	outline: none;
-	width: 200px;
+   border-radius: 8px;
+   border: none;
+   color: #ffffff;
+   cursor: pointer;
+   font-size: 16px;
+   height: 48px;
+   margin: 0 10px;
+   outline: none;
+   width: 200px;
 `;
 const App = () => (
-	<>
-		<section>
-			<h1 style={{ textAlign: "center" }}>
-				Notifications component with react-toastify
-			</h1>
-			<h4 style={{ textAlign: "center" }}>
-				<a href="https://github.com/hayitmurod707/notifications">Github</a>
-			</h4>
-			<div
-				style={{
-					alignItems: "center",
-					display: "flex",
-					justifyContent: "center",
-					margin: "20px 0",
-				}}
-			>
-				<Button
-					style={{ backgroundColor: "#0000ff" }}
-					onClick={() => notification("Info message", { type: "info" })}
-				>
-					Info
-				</Button>
-				<Button
-					style={{ backgroundColor: "#0000ff" }}
-					onClick={() =>
-						notification("Info message", {
-							autoClose: true,
-							type: "info",
-						})
-					}
-				>
-					Auto Close Info
-				</Button>
-			</div>
-			<div
-				style={{
-					alignItems: "center",
-					display: "flex",
-					justifyContent: "center",
-					margin: "20px 0",
-				}}
-			>
-				<Button
-					style={{ backgroundColor: "#ff0000" }}
-					onClick={() => notification("Error message", { type: "error" })}
-				>
-					Error
-				</Button>
-				<Button
-					style={{ backgroundColor: "#ff0000" }}
-					onClick={() =>
-						notification("Error message", {
-							autoClose: true,
-							type: "error",
-						})
-					}
-				>
-					Auto Close Error
-				</Button>
-			</div>
-			<div
-				style={{
-					alignItems: "center",
-					display: "flex",
-					justifyContent: "center",
-					margin: "20px 0",
-				}}
-			>
-				<Button
-					style={{ backgroundColor: "#ffff00" }}
-					onClick={() =>
-						notification("Warning message", {
-							type: "warning",
-						})
-					}
-				>
-					Warning
-				</Button>
-				<Button
-					style={{ backgroundColor: "#ffff00" }}
-					onClick={() =>
-						notification("Warning message", {
-							autoClose: true,
-							type: "warning",
-						})
-					}
-				>
-					Auto Close Warning
-				</Button>
-			</div>
-			<div
-				style={{
-					alignItems: "center",
-					display: "flex",
-					justifyContent: "center",
-					margin: "20px 0",
-				}}
-			>
-				<Button
-					style={{ backgroundColor: "#008000" }}
-					onClick={() =>
-						notification("Success message", {
-							type: "success",
-						})
-					}
-				>
-					Success
-				</Button>
-				<Button
-					style={{ backgroundColor: "#008000" }}
-					onClick={() =>
-						notification("Success message", {
-							autoClose: true,
-							type: "success",
-						})
-					}
-				>
-					Auto Close Success
-				</Button>
-			</div>
-			<div
-				style={{
-					alignItems: "center",
-					display: "flex",
-					justifyContent: "center",
-					margin: "20px 0",
-				}}
-			>
-				<Button
-					style={{ backgroundColor: "grey" }}
-					onClick={() =>
-						notification("Default message", {
-							type: "default",
-						})
-					}
-				>
-					Default
-				</Button>
-				<Button
-					style={{ backgroundColor: "grey" }}
-					onClick={() =>
-						notification("Default message", {
-							autoClose: true,
-							type: "default",
-						})
-					}
-				>
-					Auto Close Default
-				</Button>
-			</div>
-		</section>
-		<Notifications />
-	</>
+   <>
+      <section>
+         <h1 style={{ textAlign: 'center' }}>
+            Notifications component with react-toastify
+         </h1>
+         <h4 style={{ textAlign: 'center' }}>
+            <a href='https://github.com/hayitmurod707/notifications'>Github</a>
+         </h4>
+         <div
+            style={{
+               alignItems: 'center',
+               display: 'flex',
+               justifyContent: 'center',
+               margin: '20px 0',
+            }}
+         >
+            <Button
+               style={{ backgroundColor: '#0071f2' }}
+               onClick={() => notification('Info message', { type: 'info' })}
+            >
+               Info
+            </Button>
+            <Button
+               style={{ backgroundColor: '#0071f2' }}
+               onClick={() =>
+                  notification('Info message', {
+                     autoClose: true,
+                     type: 'info',
+                  })
+               }
+            >
+               Auto Close Info
+            </Button>
+         </div>
+         <div
+            style={{
+               alignItems: 'center',
+               display: 'flex',
+               justifyContent: 'center',
+               margin: '20px 0',
+            }}
+         >
+            <Button
+               style={{ backgroundColor: '#ea1601' }}
+               onClick={() => notification('Error message', { type: 'error' })}
+            >
+               Error
+            </Button>
+            <Button
+               style={{ backgroundColor: '#ea1601' }}
+               onClick={() =>
+                  notification('Error message', {
+                     autoClose: true,
+                     type: 'error',
+                  })
+               }
+            >
+               Auto Close Error
+            </Button>
+         </div>
+         <div
+            style={{
+               alignItems: 'center',
+               display: 'flex',
+               justifyContent: 'center',
+               margin: '20px 0',
+            }}
+         >
+            <Button
+               style={{ backgroundColor: '#fad202' }}
+               onClick={() =>
+                  notification('Warning message', {
+                     type: 'warning',
+                  })
+               }
+            >
+               Warning
+            </Button>
+            <Button
+               style={{ backgroundColor: '#fad202' }}
+               onClick={() =>
+                  notification('Warning message', {
+                     autoClose: true,
+                     type: 'warning',
+                  })
+               }
+            >
+               Auto Close Warning
+            </Button>
+         </div>
+         <div
+            style={{
+               alignItems: 'center',
+               display: 'flex',
+               justifyContent: 'center',
+               margin: '20px 0',
+            }}
+         >
+            <Button
+               style={{ backgroundColor: '#008000' }}
+               onClick={() =>
+                  notification('Success message', {
+                     type: 'success',
+                  })
+               }
+            >
+               Success
+            </Button>
+            <Button
+               style={{ backgroundColor: '#008000' }}
+               onClick={() =>
+                  notification('Success message', {
+                     autoClose: true,
+                     type: 'success',
+                  })
+               }
+            >
+               Auto Close Success
+            </Button>
+         </div>
+         <div
+            style={{
+               alignItems: 'center',
+               display: 'flex',
+               justifyContent: 'center',
+               margin: '20px 0',
+            }}
+         >
+            <Button
+               style={{ backgroundColor: 'grey' }}
+               onClick={() =>
+                  notification('Default message', {
+                     type: 'default',
+                  })
+               }
+            >
+               Default
+            </Button>
+            <Button
+               style={{ backgroundColor: 'grey' }}
+               onClick={() =>
+                  notification('Default message', {
+                     autoClose: true,
+                     type: 'default',
+                  })
+               }
+            >
+               Auto Close Default
+            </Button>
+         </div>
+      </section>
+      <Notifications />
+   </>
 );
 export default App;
--- src/Notifications.jsx
+++ src/Notifications.jsx
@@ -1,182 +1,142 @@
-import { string } from "prop-types";
-import { Bounce, toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import styled, { createGlobalStyle } from "styled-components";
-import Sound from "./notification.mp3";
-// danger color #ff0000
-// info color #0000ff
-// light color #ffffff
-// secondary color #808080
-// success color #008000
-// text color #000000
-// warning color #ffff00
+import { Bounce, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styled, { createGlobalStyle } from 'styled-components';
+import Sound from './notification.mp3';
 const defaultOptions = {
-	// autoClose: false, // false or number millisecond
-	// bodyClassName: '', // string
-	// className: '', // string
-	// closeButton: <CloseButton />, // false or ReactNode
-	// closeOnClick: true, // boolean
-	// containerId: '', // string
-	// draggable: true, // boolean
-	// draggableDirection: 'x', // x or y
-	// draggablePercent: 80, // number from 0 to 100
-	// enableMultiContainer: '', // boolean
-	// hideProgressBar: false, // boolean
-	// limit: '', // Integer number
-	// newestOnTop: false, // boolean
-	// pauseOnFocusLoss: true, // boolean
-	// pauseOnHover: true, // boolean
-	// position: 'top-right', // string
-	// progressClassName: '', // string
-	// progressStyle: {}, // object
-	// role: 'alert', // string
-	// rtl: false, // boolean
-	// style: {}, // object
-	// theme: 'light', // string oneOf ['light', 'dark']
-	// toastClassName: '', // string
-	// transition: Slide, // oneOf [Slide, Bounce, Zoom, Flip]
-	newestOnTop: true, // boolean
-	transition: Bounce, // oneOf [Slide, Bounce, Zoom, Flip]
+   // autoClose: false, // false or number millisecond
+   // bodyClassName: '', // string
+   // className: '', // string
+   // closeButton: <CloseButton />, // false or ReactNode
+   // closeOnClick: true, // boolean
+   // containerId: '', // string
+   // draggable: true, // boolean
+   // draggableDirection: 'x', // x or y
+   // draggablePercent: 80, // number from 0 to 100
+   // enableMultiContainer: '', // boolean
+   // hideProgressBar: false, // boolean
+   // limit: '', // Integer number
+   // newestOnTop: false, // boolean
+   // pauseOnFocusLoss: true, // boolean
+   // pauseOnHover: true, // boolean
+   // position: 'top-right', // string
+   // progressClassName: '', // string
+   // progressStyle: {}, // object
+   // role: 'alert', // string
+   // rtl: false, // boolean
+   // style: {}, // object
+   // theme: 'light', // string oneOf ['light', 'dark']
+   // toastClassName: '', // string
+   // transition: Slide, // oneOf [Slide, Bounce, Zoom, Flip]
+   newestOnTop: true, // boolean
+   transition: Bounce,
+   theme: 'dark',
 };
 const Styles = createGlobalStyle`
 	:root {
-		--toastify-icon-color-error: #ff0000;
-		--toastify-icon-color-info: #0000ff;
-		--toastify-icon-color-success: #008000;
-		--toastify-icon-color-warning: #ffff00;
+		--toastify-color-light: #ffffff;
+		--toastify-color-dark: #001833;
+		--toastify-color-info: #0071F2;
+		--toastify-color-success: #64BC26;
+		--toastify-color-warning: #FAD202;
+		--toastify-color-error: #EA1601;
+		--toastify-color-default: #808080;
+
+		--toastify-icon-color-info: var(--toastify-color-info);
+		--toastify-icon-color-success: var(--toastify-color-success);
+		--toastify-icon-color-warning: var(--toastify-color-warning);
+		--toastify-icon-color-error: var(--toastify-color-error);
+		
+		--toastify-text-color-info: var(--toastify-color-light);
+		--toastify-text-color-success: var(--toastify-color-light);
+		--toastify-text-color-warning: var(--toastify-color-light);
+		--toastify-text-color-error: var(--toastify-color-light);
+		
+		--toastify-color-progress-info: var(--toastify-color-info);
+		--toastify-color-progress-success: var(--toastify-color-success);
+		--toastify-color-progress-warning: var(--toastify-color-warning);
+		--toastify-color-progress-error: var(--toastify-color-error);
+		
+		--toastify-color-transparent: rgba(255, 255, 255, 0.7);
+		--toastify-toast-background: var(--toastify-color-light);
+		--toastify-text-color-light: var(--toastify-color-dark);
+		--toastify-text-color-dark: var(--toastify-color-light);
+
+		--toastify-spinner-color: var(--toastify-color-default);
+		--toastify-spinner-color-empty-area: var(--toastify-color-dark);
+		--toastify-color-progress-light: var(--toastify-color-dark);
+		--toastify-color-progress-dark: var(--toastify-color-light);
+		--toastify-shadow-light: rgba(13, 46, 105, 0.08);
+		--toastify-shadow-dark: rgba(245, 250, 255, 0.1);
 	}
 `;
 const StyledElement = styled.section`
-	& audio {
-		display: none;
-	}
-	& .Toastify__toast-container {
-		padding: 0;
-		right: 20px;
-		top: 20px;
-		& .Toastify__toast {
-			border-radius: 14px;
-			box-shadow: 0 1px 10px 0 rgba(13, 46, 105, 0.1),
-				0 1px 10px 0 rgba(13, 46, 105, 0.1);
-			color: #000000;
-			font-family: "Gilroy", sans-serif !important;
-			font-size: 15px;
-			font-weight: 600;
-			padding: 14px;
-			&:last-child {
-				margin: 0;
-			}
-			& .Toastify__toast-body {
-				margin: 0;
-				padding: 0 !important;
-				width: 100%;
-			}
-			& .Toastify__toast-close {
-				align-items: center;
-				border-radius: 50%;
-				border: none;
-				cursor: pointer;
-				display: flex;
-				height: 21px;
-				justify-content: center;
-				outline: none;
-				padding: 0;
-				position: absolute;
-				right: -10px;
-				top: -10px;
-				width: 21px;
-				&[data-type="success"] {
-					background-color: #008000;
-				}
-				&[data-type="info"] {
-					background-color: #0000ff;
-				}
-				&[data-type="error"] {
-					background-color: #ff0000;
-				}
-				&[data-type="warning"] {
-					background-color: #ffff00;
-				}
-				&[data-type="default"] {
-					background-color: #808080;
-				}
-			}
-			& .Toastify__progress-bar--warning {
-				background-color: #ffff00;
-			}
-			& .Toastify__progress-bar--success {
-				background-color: #008000;
-			}
-			& .Toastify__progress-bar--error {
-				background-color: #ff0000;
-			}
-			& .Toastify__progress-bar--info {
-				background-color: #0000ff;
-			}
-			& .Toastify__progress-bar--default {
-				background-color: #808080;
-				background-image: none;
-			}
-		}
-	}
+   & audio {
+      display: none;
+   }
+   & .Toastify__toast-container {
+      padding: 0;
+      right: 20px;
+      top: 20px;
+      &.Toastify__toast-theme--light {
+         box-shadow: 0 1px 10px 0 var(--toastify-shadow-light),
+            0 1px 10px 0 var(--toastify-shadow-light);
+      }
+      &.Toastify__toast-theme--dark {
+         box-shadow: 0 1px 10px 0 var(--toastify-shadow-dark),
+            0 1px 10px 0 var(--toastify-shadow-dark);
+      }
+      & .Toastify__toast {
+         border-radius: 14px;
+         font-family: 'Gilroy', sans-serif !important;
+         font-size: 15px;
+         font-weight: 600;
+         padding: 14px;
+         &:last-child {
+            margin: 0;
+         }
+         & .Toastify__toast-body {
+            margin: 0;
+            padding: 0 !important;
+            width: 100%;
+         }
+         & .Toastify__toast-close {
+            align-items: center;
+            border-radius: 50%;
+            border: none;
+            cursor: pointer;
+            display: flex;
+            height: 21px;
+            justify-content: center;
+            outline: none;
+            padding: 0;
+            position: absolute;
+            right: -10px;
+            top: -10px;
+            width: 21px;
+            &[data-type='success'] {
+               background-color: #64bc26;
+            }
+            &[data-type='info'] {
+               background-color: #0071f2;
+            }
+            &[data-type='error'] {
+               background-color: #ea1601;
+            }
+            &[data-type='warning'] {
+               background-color: #fad202;
+            }
+            &[data-type='default'] {
+               background-color: #808080;
+            }
+         }
+      }
+   }
 `;
-const CloseButton = ({ type }) => (
-	<button className="Toastify__toast-close" data-type={type}>
-		<svg width="10" height="10" viewBox="0 0 14 14" fill="none">
-			<path
-				d="M8.40994 7.00019L12.7099 2.71019C12.8982 2.52188 13.004 2.26649 13.004 2.00019C13.004 1.73388 12.8982 1.47849 12.7099 1.29019C12.5216 1.10188 12.2662 0.996094 11.9999 0.996094C11.7336 0.996094 11.4782 1.10188 11.2899 1.29019L6.99994 5.59019L2.70994 1.29019C2.52164 1.10188 2.26624 0.996094 1.99994 0.996094C1.73364 0.996094 1.47824 1.10188 1.28994 1.29019C1.10164 1.47849 0.995847 1.73388 0.995847 2.00019C0.995847 2.26649 1.10164 2.52188 1.28994 2.71019L5.58994 7.00019L1.28994 11.2902C1.19621 11.3831 1.12182 11.4937 1.07105 11.6156C1.02028 11.7375 0.994141 11.8682 0.994141 12.0002C0.994141 12.1322 1.02028 12.2629 1.07105 12.3848C1.12182 12.5066 1.19621 12.6172 1.28994 12.7102C1.3829 12.8039 1.4935 12.8783 1.61536 12.9291C1.73722 12.9798 1.86793 13.006 1.99994 13.006C2.13195 13.006 2.26266 12.9798 2.38452 12.9291C2.50638 12.8783 2.61698 12.8039 2.70994 12.7102L6.99994 8.41019L11.2899 12.7102C11.3829 12.8039 11.4935 12.8783 11.6154 12.9291C11.7372 12.9798 11.8679 13.006 11.9999 13.006C12.132 13.006 12.2627 12.9798 12.3845 12.9291C12.5064 12.8783 12.617 12.8039 12.7099 12.7102C12.8037 12.6172 12.8781 12.5066 12.9288 12.3848C12.9796 12.2629 13.0057 12.1322 13.0057 12.0002C13.0057 11.8682 12.9796 11.7375 12.9288 11.6156C12.8781 11.4937 12.8037 11.3831 12.7099 11.2902L8.40994 7.00019Z"
-				fill="#ffffff"
-			/>
-		</svg>
-	</button>
-);
-CloseButton.defaultProps = { type: "info" };
-CloseButton.propTypes = { type: string };
 const Notifications = () => (
-	<StyledElement>
-		<ToastContainer {...defaultOptions} />
-		<Styles />
-		<audio src={Sound} id="react-toastify-sound" />
-	</StyledElement>
+   <StyledElement>
+      <ToastContainer {...defaultOptions} />
+      <Styles />
+      <audio src={Sound} id='react-toastify-sound' />
+   </StyledElement>
 );
-export const notification = (message = "Not entered message", options = {}) => {
-	const {
-		autoClose = false,
-		type = "info",
-		onClose,
-		onOpen,
-		onClick,
-	} = options;
-	const openNotification = () => {
-		if (typeof onOpen === "function") {
-			onOpen();
-		}
-		const audio = document.getElementById("react-toastify-sound");
-		audio.currentTime = 0;
-		audio.play();
-	};
-	const option = autoClose
-		? {
-				autoClose: 5000,
-				closeButton: false,
-				onClick,
-				onClose,
-				onOpen: openNotification,
-				style: { overflow: "hidden" },
-		  }
-		: {
-				autoClose: false,
-				closeButton: <CloseButton type={type} />,
-				onClick,
-				onClose,
-				onOpen: openNotification,
-				style: { overflow: "initial" },
-		  };
-	const hasType = ["error", "info", "success", "warning"].includes(type);
-	if (hasType) {
-		toast[type](message, option);
-	} else {
-		toast(message, option);
-	}
-};
 export default Notifications;
